4376c3e7a84a6838e191437bfe64d30a5ac8eb5c
Pedro Gama <32106927+fc47081@users.noreply.github.com>
2017-11-06 12:07:10 +0000

    Update p2.py

--- p2.py
+++ p2.py
@@ -17,7 +17,6 @@ def f_aval_funcao2(estado, jogador):
     
     somatorioBrancas = 0
     somatorioPretas = 0
-
     for (_,x) in estado.board[1]['brancas']:
         somatorioBrancas += 2**(x - 2)
     for (_,x) in estado.board[1]['pretas']:
@@ -41,13 +40,9 @@ def f_aval_funcao3(estado, jogador):
     return round(0.8 * f_aval_funcao1(estado, jogador) + 0.2 * f_aval_funcao2(estado, jogador),2)
 
 
-## PARA A P4 ##  
-def f_aval_funcao3(estado, jogador):
-    return round(0.5 * f_aval_funcao1(estado, jogador) + 0.5 * f_aval_funcao2(estado, jogador),2)
-
 def n_jogos(jogo,n,jogador1,jogador2):
     estados_jogos = []
-    i = 0
+    i = 0 
     tupleList = [['empate',0],[jogador1,0],[jogador2,0]]
     if jogador1 == "F1":
         jog1 = jogador_alfabeta_1
@@ -68,7 +63,7 @@ def n_jogos(jogo,n,jogador1,jogador2):
         jog2 = random_player
         
     while i < n:
-        resultado = jogo.jogar(jog1, jog2)
+        resultado = jogo.jogar(jog1, jog2, verbose = False)
         if resultado > 0:
             tupleList[1][1] += 1
         elif resultado < 0:
@@ -88,41 +83,19 @@ def jogador_alfabeta_2(jogo,estado) :
 def jogador_alfabeta_3(jogo,estado) :
     return alphabeta_cutoff_search(estado,jogo,3,eval_fn=f_aval_funcao3)
 
-
-##def test(n):
-##    estado1 = GameState(to_move='brancas', utility=0, board=(0,
-##    {'brancas': [(1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2), (8, 2)],'pretas': [(1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7), (8, 7)]}),
-##    moves=[('avança', (1, 2)), ('avança', (2, 2)), ('avança', (3, 2)), ('avança', (4, 2)), ('avança', (5, 2)), ('avança', (6, 2)), ('avança', (7, 2)), ('avança', (8, 2))])
-##
-##    estado2 = GameState(to_move='brancas',utility=0,board=(19,
-##    {'brancas': [(2,4),(4,4),(5,5),(8,3)],'pretas':[(2,5),(3,6),(4,5),(6,5),(7,2),(8,6)]}),
-##    moves=[('avança',(5,5)),('avança',(8,3))])
-##    
-##    estado3 = GameState(to_move='brancas',utility=0,board=(19,
-##    {'brancas': [(1,2),(2,3),(3,5),(4,3),(5,2),(7,3),(8,2),(8,6)],'pretas':[(1,6),(2,7),(3,6),(4,4),(5,6),(6,7)]}),
-##    moves=[('avança',(1,6)),('avança',(2,7)),('avança',(5,6)),('avança',(6,7))])
-##    
-##    if (n == '1'):
-##        return estado1
-##    elif (n == '2'):
-##        return estado2
-##    elif (n == '3'):
-##        return estado3
-
-
 ###########################testes###########################
 
 
 ###P1,P2,P3
-#Parg = input()#"Coloque o estado que pretende: "
-#if Parg.isdigit():
-#    state = teste(int (Parg))
-#else:
-#    state = eval(Parg)
-#pieces = input()#"pretas ou brancas: "
+Parg = input()#"Coloque o estado que pretende: "
+if Parg.isdigit():
+    state = teste(int (Parg))
+else:
+    state = eval(Parg)
+pieces = input()#"pretas ou brancas: "
 #print(f_aval_funcao1(state,pieces))#p1
 #print(f_aval_funcao2(state,pieces))#p2
-#print(f_aval_funcao3(state,pieces))#p3
+print(f_aval_funcao3(state,pieces))#p3
 
 #inputFuncao1 = input()
 #inputFuncao2= input()
